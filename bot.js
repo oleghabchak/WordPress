e7049d6a7234a799c694388b2beb5930bf1cdb38
oleghabchak <71442227+oleghabchak@users.noreply.github.com>
2022-02-11 10:23:44 +0200

    update

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ bot.on("message", async msg =>{
     const chatId = msg.chat.id;
     
     if (text === "/start") {
-       await bot.sendSticker(chatId, '');
+       await bot.sendSticker(chatId, 'https://cdn.tlgrm.app/stickers/ccd/a8d/ccda8d5d-d492-4393-8bb7-e33f77c24907/192/1.webp');
        await bot.sendMessage(chatId, `Hello ${msg.from.first_name}`)
     }
 })
