f6d33f1aaab19d9686d4a338483ec3d4213ae8c0
oleghabchak <71442227+oleghabchak@users.noreply.github.com>
2022-02-11 13:42:46 +0200

    fdg

--- bot.js
+++ bot.js
@@ -1,18 +1,13 @@
 const TelegramApi = require("node-telegram-bot-api")
 const token = "1150536330:AAGOwL8xJZGXlW4B5y8ZRvyYJb2vEftOlvc" 
 
-var http = require("http");
-setInterval(function() {
-    http.get("http://sokabot6247.herokuapp.com");
-}, 300000); // every 5 minutes (300000)
-
 const bot = new TelegramApi(token, {polling:true})
 
 bot.on("message", async msg =>{
     const text = msg.text;
     const chatId = msg.chat.id;
    let a = 5; 
-    let stiker = `https://cdn.tlgrm.app/stickers/ccd/a8d/ccda8d5d-d492-4393-8bb7-e33f77c24907/192/${a}.webp`
+    let stiker = `https://tolknews.ru/picture/19141/1920x.jpg`
 
     if (text === "/start") {
        await bot.sendSticker(chatId, stiker );
@@ -20,4 +15,23 @@ bot.on("message", async msg =>{
     }
 })
 
+// отримуємо день року
+let date365 = 0;
 
+let currentDate = new Date();
+let currentYear = currentDate.getFullYear();
+let currentMonth = currentDate.getMonth(); 
+let currentDay = currentDate.getDate(); 
+
+let monthLength = [31,28,31,30,31,30,31,31,30,31,30,31];
+
+let leapYear = new Date(currentYear, 1, 29); 
+if (leapYear.getDate() == 29) { // If it's a leap year, changes 28 to 29
+    monthLength[1] = 29;
+}
+
+for ( i=0; i < currentMonth; i++ ) { 
+    date365 = date365 + monthLength[i];
+}
+date365 = date365 + currentDay; 
+console.log(date365);
